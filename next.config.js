8c9dbf4e6999171e61254fb4c7a86edd768b8143
Igor Bedesqui <igorbedesqui@gmail.com>
2022-02-02 00:43:50 -0300

    THIS FUCKING WORKSSSSSS

--- next.config.js
+++ next.config.js
@@ -1,16 +1,26 @@
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en", "pt"],
+    defaultLocale: "en",
+  },
 };
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+// const withMDX = require("@next/mdx")({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [],
+//     rehypePlugins: [],
+//   },
+// });
+
+module.exports = withContentlayer()({
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "pt"],
+    defaultLocale: "en",
   },
-});
-module.exports = withMDX({
-  ...nextConfig,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 });
---
+++ pages/projects/[slug].tsx
@@ -0,0 +1,71 @@
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { allProjects } from ".contentlayer/data";
+import type { Project } from ".contentlayer/types";
+import { useEffect } from "react";
+import type { GetStaticPathsContext, GetStaticPathsResult } from "next";
+
+export async function getStaticPaths({
+  locales,
+}: GetStaticPathsContext): Promise<GetStaticPathsResult> {
+  let paths: { params: { slug: string }; locale: string }[] = [];
+  locales?.forEach((locale) => {
+    let localeProjects = allProjects.filter((p) => p.lang == locale);
+
+    let temp = localeProjects.map((p) => ({
+      params: { slug: p.slug },
+      locale: locale,
+    }));
+
+    paths.push(...temp);
+  });
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const project = allProjects.find((project) => project.slug === params.slug);
+
+  return { props: { project } };
+}
+
+const ProjectLayout = ({ project }: { project: Project }) => {
+  const MDXContent = useMDXComponent(project.body.code);
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router);
+  }, [router]);
+
+  return (
+    <>
+      <Head>
+        <title>{project.title}</title>
+      </Head>
+      <article>
+        <Link
+          href={`${project?.alternate?.slug}`}
+          locale={project?.alternate?.locale}
+        >{`${project?.alternate?.slug}`}</Link>
+        <button
+          onClick={() => {
+            console.log(router.locale);
+          }}
+        >
+          Hej
+        </button>
+        <div>
+          <h1>{project.title}</h1>
+        </div>
+        <MDXContent />
+      </article>
+    </>
+  );
+};
+
+export default ProjectLayout;
