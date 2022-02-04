29c4724570a7e494d76ae3195c467da96da56f13
Igor Bedesqui <igorbedesqui@gmail.com>
2022-02-04 20:44:12 -0300

    refactor: removed unused declaration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 const { withContentlayer } = require("next-contentlayer");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ["en", "pt"],
-    defaultLocale: "en",
-  },
-};
-
 // const withMDX = require("@next/mdx")({
 //   extension: /\.mdx?$/,
 //   options: {
@@ -17,6 +8,9 @@ const nextConfig = {
 //   },
 // });
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = withContentlayer()({
   reactStrictMode: true,
   i18n: {
