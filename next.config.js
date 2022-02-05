d20daaf4cc6ca44fb5a8fbd825ab8fdf6b02f85c
Igor Bedesqui <igorbedesqui@gmail.com>
2022-02-04 21:58:57 -0300

    build: setup preact

--- next.config.js
+++ next.config.js
@@ -21,11 +21,10 @@ module.exports = withContentlayer()({
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
     if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
+      // IGOR, THIS WILL PROBABLY BREAK AT SOME POINT, IT IS A WORKAROUND, USUALLY jsx-runtime DOESN'T NEED THE .js
+      config.resolve.alias["react/jsx-runtime.js"] = "preact/jsx-runtime";
+      config.resolve.alias["react"] = "preact/compat";
+      config.resolve.alias["react-dom"] = "preact/compat";
     }
 
     return config;
